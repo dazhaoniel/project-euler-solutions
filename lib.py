62630c47c15525cc7278ffdb5bb29be857b3ef33
Daniel Zhao <jingyi@squareup.com>
2022-09-01 00:40:17 -0400

    p35

--- lib.py
+++ lib.py
@@ -1,5 +1,21 @@
+import math
+
 def is_prime(n):
 	for i in range(2, n // 2 + 1):
 		if (n % i == 0):
 			return False
-	return True
+			return True
+
+def sieve(n):
+	primes = [True] * (n+1)
+	primes[0] = primes[1] = False
+
+	for x in range(4,n+1,2):
+		primes[x] = False
+
+		for x in range(3,int(math.sqrt(n))+1,2):
+			if(primes[x]):
+				for y in range(x*x,n+1,x):
+					primes[y] = False
+
+					return primes
--- p035.py
+++ p035.py
@@ -1,25 +1,29 @@
 # Circular primes
 # https://projecteuler.net/problem=35
-from lib import is_prime
+import numpy as np
+import math
+
+# http://rebrained.com/?p=458
+def prime3(upto=1000000):
+	return [2] + list(filter(lambda num: (num % np.arange(3,1+int(math.sqrt(num)),2)).all(), range(3,upto+1,2)))
 
 def rotation(number):
-    str_number = str(number)
-    return {
-        int(str_number[i:] + str_number[:i])
-        for i in range(len(str_number))
-    }
+	str_number = str(number)
+	return {
+	int(str_number[i:] + str_number[:i])
+	for i in range(len(str_number))
+	}
 
 def circular_primes():
 	count = 0
-	
-	for i in range(2, 1000000):
-		if is_prime(i):
-			count += 1
-			numbers_list = list(rotation(i))
-			for num in numbers_list:
-				if not is_prime(num):
-					count -= 1
-					break
+	prime = prime3()
+	for i in prime:
+		count += 1
+		numbers_list = list(rotation(i))
+		for num in numbers_list:
+			if num not in prime:
+				count -= 1
+				break
 
 	return count
 
