a2919c79d7703c621495a993774f6a03eb5c4992
Daniel Zhao <danielantoiny@gmail.com>
2022-08-31 23:41:18 -0400

    p35

--- p035.py
+++ p035.py
@@ -1,2 +1,27 @@
 # Circular primes
 # https://projecteuler.net/problem=35
+from lib import is_prime
+
+def rotation(number):
+    str_number = str(number)
+    return {
+        int(str_number[i:] + str_number[:i])
+        for i in range(len(str_number))
+    }
+
+def circular_primes():
+	count = 0
+	
+	for i in range(2, 1000000):
+		if is_prime(i):
+			count += 1
+			numbers_list = list(rotation(i))
+			for num in numbers_list:
+				if not is_prime(num):
+					count -= 1
+					break
+
+	return count
+
+if __name__ == '__main__':
+	print(circular_primes())
