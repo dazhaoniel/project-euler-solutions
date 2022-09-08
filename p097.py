0cbbf268d1c5b809d83f44027399eed86657438b
Daniel Zhao <jingyi@squareup.com>
2022-09-08 09:46:11 -0400

    add comment p97

--- p097.py
+++ p097.py
@@ -1,6 +1,8 @@
 # Large non-Mersenne prime
 # https://projecteuler.net/problem=97
 def large_non_mersenne_prime():
+	# pow(number, power, modulus [optional])
+	# 10 ** 10 for the last 10 digits
 	return (28433 * pow(2, 7830457, 10 ** 10) + 1) % 10 ** 10
 
 if __name__ == '__main__':
